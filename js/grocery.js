7f51267f8e356cfdb5ffeb2bbbc48f9574ec8507
Stephen <szook7@gmail.com>
2023-01-17 13:52:35 -0800

    fixed list to display correct values with for loop
    Co-authored-by: viradeth <xayananh@gmail.com>

--- js/grocery.js
+++ js/grocery.js
@@ -1,43 +1,40 @@
-window.addEventListener("load", function() {
-  // document.querySelector("form#defaultHidden").removeAttribute("class");
+window.addEventListener("load", function () {
+  // document.querySelector("form#defaultHidden").removeAttribute("class");  
   
-  const form =  this.document.querySelector("form");
+  const form = this.document.querySelector("form");
   form.removeAttribute("class")
-  
-  form.addEventListener("submit", function(event) {
+  const div = document.querySelector("div")
+
+  form.addEventListener("submit", function (event) {
     event.preventDefault()
-   
     let firstInput = document.getElementById('firstInput').value;
     let secondInput = document.getElementById('secondInput').value;
     let thirdInput = document.getElementById("thirdInput").value;
-
-    let selectionArray = [firstInput, secondInput, thirdInput];   
-    console.log(selectionArray)
-    // let firstUlElement = document.createElement("ul")
-
-    var firstUlElement = document.createElement("ul");
- 
     
- 
-  
-    
-    
-    
-    selectionArray.forEach(function(element){
-      
-      let firstLiElement = document.createElement("li");
-      firstUlElement.appendChild(firstLiElement);
+    let selectionArray = [firstInput, secondInput, thirdInput];
 
-      firstLiElement.innerHTML = firstLiElement.innerHTML + element;
-      
-    });
-    // document.body.appendChild(firstUlElement);
+    function makeUL(array) {
+      // Create the list element:
+      var firstUlElement = document.createElement('ul');
 
-    // document.body.append(firstUlElement);
+      for (var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var firstliElement = document.createElement('li');
 
-     
+        // Set its contents:
+        firstliElement.appendChild(document.createTextNode(array[i]));
 
-  
+        // Add it to the list:
+        firstUlElement.appendChild(firstliElement);
+      }
+
+      // Finally, return the constructed list:
+      return firstUlElement;
+    }
+    // Add the contents of options[0] to #foo:
+    document.getElementById('foo').appendChild(makeUL(selectionArray));
+    div.removeAttribute("class")
+
   });
 
 });
